945a885950bf77a98842b57d86c38c7eff6fd99b
GabrielCampelo1998 <gabrielcampelo98@outlook.com>
2025-11-06 12:06:53 -0300

    :sparkles: feat: adiciona tela de login

---
+++ src/app/components/Login.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Login() {
+  return (
+    <div className="w-screen h-screen bg-white px-16 py-16 flex justify-center items-center">
+      <div className="bg-white w-full gap-4 border shadow p-4 rounded-2xl flex flex-row justify-between">
+        <div className="bg-pink-100 rounded-2xl border">
+          <Image
+            alt="cat with tools"
+            src={"/img/cat.png"}
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className="flex-1 p-4 gap-3 flex flex-col justify-center items-center">
+          <h1 className="text-black text-2xl font-semibold">
+            Faça Login em sua conta
+          </h1>
+          <h3 className="text-black text-sm font-medium">
+            Não possui conta ?{" "}
+            <Link className="text-blue-500 underline" href={"/"}>
+              Crie uma aqui
+            </Link>
+          </h3>
+          <form action="POST" className="flex flex-col gap-4 w-full  px-35">
+            <div className=" flex flex-col gap-2">
+              <label className="text-black text-sm">Email</label>
+              <input
+                placeholder="email@mail.com"
+                type="email"
+                className="border p-1 transition-all duration-700 w-1/2  focus:w-full bg-gray-50 border-gray-200 rounded-2xl text-gray-800 px-4 flex justify-center items-center"
+              />
+            </div>
+            <div className=" flex flex-col gap-2">
+              <label className="text-black text-sm">Senha</label>
+              <input
+                placeholder="********"
+                type="password"
+                className="border transition-all w-1/2 duration-700 focus:w-full bg-gray-50 border-gray-200 p-1  rounded-2xl text-gray-800 px-4 flex justify-center items-center align-middle"
+              />
+            </div>
+            <div className="flex flex-row gap-2">
+              <input type="checkbox" />
+              <span className="text-black text-sm">Lembrar-me</span>
+            </div>
+            <button className=" bg-gray-900 rounded-4xl p-2 hover:cursor-pointer hover:bg-black">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
