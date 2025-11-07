794826fd0e6c9093d7ef73a399b527bfcfd1f472
GabrielCampelo1998 <gabrielcampelo98@outlook.com>
2025-11-06 21:27:46 -0300

    styles: adiciona novos estilos e elementos

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import Form from "next/form";
 
 function Login() {
   return (
@@ -14,22 +15,25 @@ function Login() {
           />
         </div>
         <div className="flex-1 p-4 gap-3 flex flex-col justify-center items-center">
-          <h1 className="text-black text-2xl font-semibold">
+          <h1 className="text-black text-3xl font-semibold">
             Faça Login em sua conta
           </h1>
-          <h3 className="text-black text-sm font-medium">
-            Não possui conta ?{" "}
+          <h3 className="text-gray-700 text-sm font-medium">
+            Não possui conta?{" "}
             <Link className="text-blue-500 underline" href={"/"}>
               Crie uma aqui
             </Link>
           </h3>
-          <form action="POST" className="flex flex-col gap-4 w-full  px-35">
+          <Form action="/" className="flex flex-col gap-4 w-full px-35">
             <div className=" flex flex-col gap-2">
               <label className="text-black text-sm">Email</label>
               <input
-                placeholder="email@mail.com"
+                placeholder="seuemail@exemplo.com.br"
                 type="email"
-                className="border p-1 transition-all duration-700 w-1/2  focus:w-full bg-gray-50 border-gray-200 rounded-2xl text-gray-800 px-4 flex justify-center items-center"
+                className="border shadow p-1 transition-all duration-700 w-3/4 focus:w-full bg-gray-50 border-gray-300 rounded-2xl text-gray-800 px-4"
+                autoComplete="off"
+                readOnly
+                onFocus={(e) => e.target.removeAttribute("readonly")}
               />
             </div>
             <div className=" flex flex-col gap-2">
@@ -37,17 +41,20 @@ function Login() {
               <input
                 placeholder="********"
                 type="password"
-                className="border transition-all w-1/2 duration-700 focus:w-full bg-gray-50 border-gray-200 p-1  rounded-2xl text-gray-800 px-4 flex justify-center items-center align-middle"
+                className="border shadow transition-all w-3/4 duration-700 focus:w-full bg-gray-50 border-gray-300 p-1  rounded-2xl text-gray-800 px-4 flex justify-center items-center"
               />
             </div>
             <div className="flex flex-row gap-2">
               <input type="checkbox" />
               <span className="text-black text-sm">Lembrar-me</span>
             </div>
-            <button className=" bg-gray-900 rounded-4xl p-2 hover:cursor-pointer hover:bg-black">
+            <button
+              type="submit"
+              className=" bg-gray-950 rounded-4xl p-2 hover:cursor-pointer hover:bg-black"
+            >
               Login
             </button>
-          </form>
+          </Form>
         </div>
       </div>
     </div>
