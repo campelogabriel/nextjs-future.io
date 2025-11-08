adac6fd44209b788b2720c04b75c1aeb668c39aa
GabrielCampelo1998 <gabrielcampelo98@outlook.com>
2025-11-08 12:08:46 -0300

    :bug: style: corrigindo bug da cor do texto

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 function Footer() {
   return (
-    <div className="bg-black p-4 flex flex-row gap-4 justify-center items-center absolute z-50 bottom-0 left-0 w-full">
+    <div className="bg-black text-white p-4 flex flex-row gap-4 justify-center items-center absolute z-50 bottom-0 left-0 w-full">
       <h1 className={`${inter.className}`}>
         © 2025 Gabriel Campelo. Todos os direitos reservados.
       </h1>
