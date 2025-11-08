27337e9f37470defec86faf6812e130e9978a2f6
GabrielCampelo1998 <gabrielcampelo98@outlook.com>
2025-11-08 15:34:20 -0300

    :bug: style: corrigindo bug da posiçao

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,9 +2,13 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-function Footer() {
+function Footer({ position }) {
   return (
-    <div className="bg-black text-white p-4 flex flex-row gap-4 justify-center items-center absolute z-50 bottom-0 left-0 w-full">
+    <div
+      className={`bg-black text-white p-4 flex flex-row gap-4 justify-center items-center  z-50 bottom-0 left-0 w-full ${
+        position ? "absolute" : "static"
+      }`}
+    >
       <h1 className={`${inter.className}`}>
         © 2025 Gabriel Campelo. Todos os direitos reservados.
       </h1>
