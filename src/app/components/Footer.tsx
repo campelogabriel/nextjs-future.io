b728f49ca56480080171ed1b4813b5fd7143bd88
GabrielCampelo1998 <gabrielcampelo98@outlook.com>
2025-11-09 11:51:30 -0300

    styles: adiciona estilo responsivel

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,16 +1,20 @@
+"use client";
+
 import { Inter } from "next/font/google";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function Footer({ position }) {
+  const [date, setData] = useState<Date>(new Date());
   return (
     <div
-      className={`bg-black text-white p-4 flex flex-row gap-4 justify-center items-center  z-50 bottom-0 left-0 w-full ${
+      className={`bg-black text-white p-4 flex flex-row gap-4 justify-center items-center z-50 bottom-0 left-0 w-full ${
         position ? "absolute" : "static"
       }`}
     >
-      <h1 className={`${inter.className}`}>
-        © 2025 Gabriel Campelo. Todos os direitos reservados.
+      <h1 className={`${inter.className} text-xs xs:text-sm sm:text-lg`}>
+        © {date.getFullYear()} Gabriel Campelo. Todos os direitos reservados.
       </h1>
     </div>
   );
